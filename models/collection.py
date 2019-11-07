3655442261e151bac59f0453cfb63595c0e99b6d
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-07 12:47:50 +0000

    UPDATE: Collection to work with the Site class

--- models/collection.py
+++ models/collection.py
@@ -1,4 +1,22 @@
-class Collection:
+from models.site import Site
 
+class Collection:
+    # An instance of the Site class.
+    site = ''
+
+    # The language picked to view the site.
+    language = ''
+
+    # Constructor.
+    def __init__(self):
+        self.site = Site()
+        self.language = self.site.language()
+
+    # Returns the collection total value converted into the right country currency.
     def totalValue(self):
-        return ''
+        if self.language == 'esp':
+            self.currency = '€'
+        else:
+            self.currency = '£'
+
+        return self.currency
