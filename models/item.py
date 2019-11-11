ae153eb895dbf2528e0fd3c3e526e0e2c6572dba
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-11 17:01:56 +0000

    UPDATE: Tests to work when passing strings without separator

--- models/item.py
+++ models/item.py
@@ -8,6 +8,7 @@ class Item:
     # Google Sheet information containing the data for the Item.
     googleData = ''
 
+
     # Constructor.
     def __init__(self, language, googleData):
         self.language = language
@@ -16,52 +17,64 @@ class Item:
         # {field:value, field:value}, {field:value, field:value}, {...
         self.googleData = googleData
 
+
     # Returns the item back image URL.
     def backImage(self):
         return splitByLanguage(self.googleData['Image Dorso | Back Image'], self.language)
+
 
     # Returns the item cost when it was initially bought.
     def cost(self):
         return splitByLanguage(self.googleData['Coste | Cost'], self.language)
 
+
     # Returns the item country.
     def country(self):
         return splitByLanguage(self.googleData['País | Country'], self.language)
+
 
     # Returns the item date of issue.
     def date(self):
         return splitByLanguage(self.googleData['Fecha de fabricación | Date of issue'], self.language)
 
+
     # Returns the item currency.
     def denomination(self):
         return splitByLanguage(self.googleData['Denominación | Denomination'], self.language)
+
 
     # Returns the item front image URL.
     def frontImage(self):
         return splitByLanguage(self.googleData['Image Cara | Front Image'], self.language)
 
+
     # Returns the item grading.
     def grading(self):
         return splitByLanguage(self.googleData['Estado | Grading'], self.language)
 
+
     # Returns the item name.
     def name(self):
         return splitByLanguage(self.googleData['Nombre | Name'], self.language)
+
 
     # Returns the item serial number.
     # Only bank notes have one.
     def serialNumber(self):
         return splitByLanguage(self.googleData['Número de Serie | Serial numbers'], self.language)
 
+
     # Returns the item series number.
     # Only bank notes have one.
     def seriesNumber(self):
         return splitByLanguage(self.googleData['Año de la serie | Series number'], self.language)
 
+
     # Returns the item type.
     # It would Bank Note or Coin.
     def type(self):
         return splitByLanguage(self.googleData['Tipo | Type'], self.language)
+
 
     # Returns the item value.
     def value(self):
--- tests/models/tests-item.py
+++ tests/models/tests-item.py
@@ -1,6 +1,7 @@
 import pytest
 from models.item import Item
 
+# Faked google sheet data to be used in the tests.
 def googleData():
     return {
         'Tipo | Type': 'Moneda | Coin',
@@ -17,28 +18,66 @@ def googleData():
         'Coste | Cost': '6,99 EUR',
     }
 
+# Wrapper to avoid repetition in the Item tests.
+def wrapper(method, dataName):
+    data = googleData()
+    item = Item('esp', data)
+
+    if data[dataName].find(' | ') >= 0:
+        assert getattr(item, method)() == (data[dataName].split(' | '))[0]
+
+        item.language = 'eng'
+        assert getattr(item, method)() == (data[dataName].split(' | '))[1]
+    else:
+        assert getattr(item, method)() == data[dataName]
+
+
 class TestItem:
 
-    def test_item_returns_expected_type_in_spanish(self):
-        data = googleData()
-        item = Item('esp', data)
 
-        assert item.type() == (data['Tipo | Type'].split(' | '))[0]
+    def test_item_returns_expected_type(self):
+        wrapper('type', 'Tipo | Type')
 
-    def test_item_returns_expected_type_in_english(self):
-        data = googleData()
-        item = Item('eng', data)
 
-        assert item.type() == (data['Tipo | Type'].split(' | '))[1]
+    def test_item_returns_expected_name(self):
+        wrapper('name', 'Nombre | Name')
 
-    def test_item_returns_expected_name_in_spanish(self):
-        data = googleData()
-        item = Item('esp', data)
 
-        assert item.name() == (data['Nombre | Name'].split(' | '))[0]
+    def test_item_returns_expected_front_image(self):
+        wrapper('frontImage', 'Image Cara | Front Image')
 
-    def test_item_returns_expected_name_in_english(self):
-        data = googleData()
-        item = Item('eng', data)
 
-        assert item.name() == (data['Nombre | Name'].split(' | '))[1]
+    def test_item_returns_expected_back_image(self):
+        wrapper('backImage', 'Image Dorso | Back Image')
+
+
+    def test_item_returns_expected_country(self):
+        wrapper('country', 'País | Country')
+
+
+    def test_item_returns_expected_denomination(self):
+        wrapper('denomination', 'Denominación | Denomination')
+
+
+    def test_item_returns_expected_date(self):
+        wrapper('date', 'Fecha de fabricación | Date of issue')
+
+
+    def test_item_returns_expected_series_number(self):
+        wrapper('seriesNumber', 'Año de la serie | Series number')
+
+
+    def test_item_returns_expected_serial_number(self):
+        wrapper('serialNumber', 'Número de Serie | Serial numbers')
+
+
+    def test_item_returns_expected_grading(self):
+        wrapper('grading', 'Estado | Grading')
+
+
+    def test_item_returns_expected_value(self):
+        wrapper('value', 'Valor | Value')
+
+
+    def test_item_returns_expected_cost(self):
+        wrapper('cost', 'Coste | Cost')
