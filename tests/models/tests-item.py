3134fee5df85ef70b7768ec4c7b053daca1541d2
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-11 12:44:42 +0000

    ADD: Initial tests for item class

--- tests/models/tests-item.py
+++ tests/models/tests-item.py
@@ -12,14 +12,33 @@ def googleData():
         'Fecha de fabricación | Date of issue': '1992',
         'Año de la serie | Series number': '',
         'Número de Serie | Serial numbers': '',
-        'Estado | Grading': '',
+        'Estado | Grading': 'EBC (Extraordinariamente Bien Conservada) | EF (Extremely Fine)',
         'Valor | Value': '2 EUR',
         'Coste | Cost': '6,99 EUR',
     }
 
 class TestItem:
 
-    def test_item_returns_expected_name_in_spanish(self):
-        item = Item('esp', googleData())
+    def test_item_returns_expected_type_in_spanish(self):
+        data = googleData()
+        item = Item('esp', data)
 
-        assert item.name() == []
+        assert item.type() == (data['Tipo | Type'].split(' | '))[0]
+
+    def test_item_returns_expected_type_in_english(self):
+        data = googleData()
+        item = Item('eng', data)
+
+        assert item.type() == (data['Tipo | Type'].split(' | '))[1]
+
+    def test_item_returns_expected_name_in_spanish(self):
+        data = googleData()
+        item = Item('esp', data)
+
+        assert item.name() == (data['Nombre | Name'].split(' | '))[0]
+
+    def test_item_returns_expected_name_in_english(self):
+        data = googleData()
+        item = Item('eng', data)
+
+        assert item.name() == (data['Nombre | Name'].split(' | '))[1]
