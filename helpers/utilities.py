e4086297fd91d9edc000ead30ef238540f49926b
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-11 16:54:06 +0000

    UPDATE: splitByLanguage to be able to handle strings without separator

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -1,6 +1,10 @@
-# Split the Google Data returning the one requested in the view based on it's language.
+# Split the Google Data returning the one requested in the view based on it's language
+# if the data received is divided by |.
 def splitByLanguage(googleData, language):
-    if language == 'eng':
-        return (googleData.split(' | '))[1]
+    if googleData.find(' | ') >= 0:
+        if language == 'eng':
+            return (googleData.split(' | '))[1]
 
-    return (googleData.split(' | '))[0]
+        return (googleData.split(' | '))[0]
+    else:
+        return googleData
