ba58354008eecedf9b7eb7e63885c81094d4d389
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-11 12:08:33 +0000

    UPDATE: Item methods to look for the right place in the Google data dictionary

--- models/item.py
+++ models/item.py
@@ -1,60 +1,66 @@
 class Item:
+    # The language picked to view the site.
+    # The default view is Spanish.
+    language = 'esp'
+
     # Google Sheet information containing the data for the Item.
     googleData = ''
 
     # Constructor.
-    def __init__(self, googleData):
+    def __init__(self, language, googleData):
+        self.language = language
+
         # googleData sctructure:
         # {field:value, field:value}, {field:value, field:value}, {...
         self.googleData = googleData
 
     # Returns the item back image URL.
     def backImage(self):
-        return ''
+        return self.googleData['Image Dorso | Back Image']
 
     # Returns the item cost when it was initially bought.
     def cost(self):
-        return ''
+        return self.googleData['Coste | Cost']
 
     # Returns the item country.
     def country(self):
-        return ''
+        return self.googleData['País | Country']
+
+    # Returns the item date of issue.
+    def date(self):
+        return self.googleData['Fecha de fabricación | Date of issue']
 
     # Returns the item currency.
     def denomination(self):
-        return ''
+        return self.googleData['Denominación | Denomination']
 
     # Returns the item front image URL.
     def frontImage(self):
-        return ''
+        return self.googleData['Image Cara | Front Image']
 
     # Returns the item grading.
     def grading(self):
-        return ''
+        return self.googleData['Estado | Grading']
 
     # Returns the item name.
     def name(self):
-        return ''
+        return self.googleData['Nombre | Name']
 
     # Returns the item serial number.
     # Only bank notes have one.
     def serialNumber(self):
-        return ''
+        return self.googleData['Número de Serie | Serial numbers']
 
     # Returns the item series number.
     # Only bank notes have one.
     def seriesNumber(self):
-        return ''
+        return self.googleData['Año de la serie | Series number']
 
     # Returns the item type.
     # It would Bank Note or Coin.
     def type(self):
-        return ''
-
-    # Returns the item creation year.
-    def year(self):
-        return ''
+        return self.googleData['Tipo | Type']
 
     # Returns the item value.
     def value(self):
-        return ''
+        return self.googleData['Valor | Value']
--- tests/models/tests-item.py
+++ tests/models/tests-item.py
@@ -1,9 +1,25 @@
 import pytest
 from models.item import Item
 
+def googleData():
+    return {
+        'Tipo | Type': 'Moneda | Coin',
+        'Nombre | Name': 'Nombre del objeto | Item name',
+        'Image Cara | Front Image': '',
+        'Image Dorso | Back Image': '',
+        'País | Country': 'Reino Unido | United Kingdom',
+        'Denominación | Denomination': '2 EUR',
+        'Fecha de fabricación | Date of issue': '1992',
+        'Año de la serie | Series number': '',
+        'Número de Serie | Serial numbers': '',
+        'Estado | Grading': '',
+        'Valor | Value': '2 EUR',
+        'Coste | Cost': '6,99 EUR',
+    }
+
 class TestItem:
 
-    def test_item_returns_expected_total_value_in_spanish(self):
-        item = Item([])
+    def test_item_returns_expected_name_in_spanish(self):
+        item = Item('esp', googleData())
 
         assert item.name() == []
