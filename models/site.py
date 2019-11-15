0d657dfefefb2b56eb20397c42278a9108e77ff3
Jesús A. Loranca Rubio <jesus@substrakt.com>
2019-11-15 12:41:37 +0000

    UPDATE: Site class to handle language, name and title

--- models/site.py
+++ models/site.py
@@ -1,8 +1,19 @@
 class Site:
+    # The language picked to view the site.
+    # The default view is Spanish.
+    language = 'es'
 
-    # Returns the language picked to view the site.
-    def language(self):
-        if False:
-            return 'eng'
+    # The page name.
+    # It would be different depending on the view and the language.
+    pageName = 'es'
 
-        return 'esp'
+    # Constructor.
+    def __init__(self, language, pageName):
+        self.language = language
+        self.title = pageName
+
+    def name(self):
+        if self.language == 'en':
+            return 'Numismatic Collection'
+
+        return 'Colección Numismática'
