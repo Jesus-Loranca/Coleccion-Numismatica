9d1f6b4d37253fa882c0169d5c4a82a8ce518340
Jesus A. Loranca Rubio <jesus.loranca@hroc.co.uk>
2020-03-06 14:16:17 +0000

    UPDATE: Google Spreadsheet connection to happen in a method

--- models/collection.py
+++ models/collection.py
@@ -6,44 +6,6 @@ from models.site import Site
 from models.item import Item
 
 class Collection:
-    # Use credentials to create a client to interact with the Google Drive API
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
-    # Private key is being read wrong from the .env so its value have to be replaced
-    # Locally spaces are being translated as \\\\n and heroku translates them as \\n
-    privateKey = os.getenv('private_key')
-
-    if os.getenv('environment') == 'local':
-        privateKey = privateKey.replace('\\\\n', '\n')
-
-    if os.getenv('environment') == 'production':
-         privateKey = privateKey.replace('\\n', '\n')
-
-    credentialsFile = {
-        'type':                        os.getenv('type'),
-        'project_id':                  os.getenv('project_id'),
-        'private_key_id':              os.getenv('private_key_id'),
-        'private_key':                 privateKey,
-        'client_email':                os.getenv('client_email'),
-        'client_id':                   os.getenv('client_id'),
-        'auth_uri':                    os.getenv('auth_uri'),
-        'token_uri'                  : os.getenv('token_uri'),
-        'auth_provider_x509_cert_url': os.getenv('auth_provider_x509_cert_url'),
-        'client_x509_cert_url':        os.getenv('client_x509_cert_url')
-    }
-
-    # In the case the credetails need to be read from a file:
-    # credentials = ServiceAccountCredentials.from_json_keyfile_name('Coleccion Numismatica-3639760c8254.json', scope)
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentialsFile, scope)
-    client = gspread.authorize(credentials)
-
-    # Find a workbook by name and open the first sheet.
-    sheet = client.open('Colección Numismática').sheet1
-
-    # Extract the collection values.
-    googleData = sheet.get_all_records()
-
     # An instance of the Site class.
     site = ''
 
@@ -55,20 +17,24 @@ class Collection:
     # Defaults to EUR as the default view would be Spanish.
     currency = 'EUR'
 
+    # Google Spreadsheet information.
+    googleData = ''
+
     # Constructor.
     def __init__(self, site):
         self.site = site
         self.language = self.site.language
 
-        # Retrieves the google spreadsheet data.
+        # Retrieves the Google Spreadsheet data.
+        self.googleSpreadsheet()
 
-        # Conects to smartfile to retrieve the images.
+        # Conects to SmartFile to retrieve the images.
         self.smartFile()
 
         if self.language == 'en':
             self.currency = 'GBP'
 
-    # Returns all the collection being readed from the google spreadsheet.
+    # Returns all the collection being readed from the Google Spreadsheet.
     def all(self):
         return self.googleData
 
@@ -82,7 +48,7 @@ class Collection:
 
         return collectionAsItems
 
-    # Finds an item from the google spreadsheet data based on its name.
+    # Finds an item from the Google Spreadsheet data based on its name.
     def find(self, name = '', field = 'Nombre | Name'):
         return next(
             (
@@ -91,7 +57,45 @@ class Collection:
             ), {}
         )
 
-    # Returns the smartfile connection to be able to use it later on for images.
+    # Authenticates against the Google Spreadsheet to retrieve its information.
+    def googleSpreadsheet(self):
+        # Use credentials to create a client to interact with the Google Drive API
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+        # Private key is being read wrong from the .env so its value have to be replaced
+        # Locally spaces are being translated as \\\\n and heroku translates them as \\n
+        privateKey = os.getenv('private_key')
+
+        if os.getenv('environment') == 'local':
+            privateKey = privateKey.replace('\\\\n', '\n')
+
+        if os.getenv('environment') == 'production':
+            privateKey = privateKey.replace('\\n', '\n')
+
+        credentialsFile = {
+            'type':                        os.getenv('type'),
+            'project_id':                  os.getenv('project_id'),
+            'private_key_id':              os.getenv('private_key_id'),
+            'private_key':                 privateKey,
+            'client_email':                os.getenv('client_email'),
+            'client_id':                   os.getenv('client_id'),
+            'auth_uri':                    os.getenv('auth_uri'),
+            'token_uri'                  : os.getenv('token_uri'),
+            'auth_provider_x509_cert_url': os.getenv('auth_provider_x509_cert_url'),
+            'client_x509_cert_url':        os.getenv('client_x509_cert_url')
+        }
+
+        # In the case the credetails need to be read from a file:
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentialsFile, scope)
+        client = gspread.authorize(credentials)
+
+        # Find a workbook by name and open the first sheet.
+        sheet = client.open('Colección Numismática').sheet1
+
+        # Extract the collection values.
+        self.googleData = sheet.get_all_records()
+
+    # Returns the SmartFile connection to be able to use it later on for images.
     def smartFile(self):
         smartFile = BasicClient(os.getenv('smartfile_api_key'), os.getenv('smartfile_api_password'))
         print(smartFile.get('/ping'))
