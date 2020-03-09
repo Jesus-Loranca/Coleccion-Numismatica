3e2b47c1ae41051be4c2aaad5d5521065605b94e
Jesus A. Loranca Rubio <jesus.loranca@hroc.co.uk>
2020-03-09 14:00:44 +0000

    ADD: Initial files upload to SmartFile from the form

--- app.py
+++ app.py
@@ -37,39 +37,48 @@ def form(language = 'es'):
     if request.method == 'POST':
         # Upload the images first to SmartFile and once we get the URLs for the images
         # we will update our Google Spreadsheet to include that URLs.
-        if request.form.get('obverse') or request.form.get('reverse'):
+        if not request.files['obverse'] or not request.files['reverse']:
             return render_template('form-error.html', site = site)
         else:
-            # filename = secure_filename(request.form.get('obverse'))
-            # collection.smartFileClient.put('/path/oper/mkdir/Items/Coin/United Kingdom/1997')
+            # Builds the folder structure to store the image.
+            filePath = 'Items/' +  splitByLanguage(request.form.get('type'), 'en') + '/' +  splitByLanguage(request.form.get('country'), 'en') + '/' +  splitByLanguage(request.form.get('date'), 'en')
 
-            obverse = ''
-            reverse = ''
+            obverse = collection.smartFileClient.post('/path/data/' + filePath, file = (request.files['obverse'].filename, request.files['obverse']))
+
+            print(obverse)
+
+            # [{'id': 252129318, 'name': 'image3.jpg', 'path': '/Items/Coin/United Kingdom/1997/image3.jpg', 'url': '/api/2.1/path/info/Items/Coin/United%20Kingdom/1997/image3.jpg', 'links': 1, 'remote_status': None, 'size': 169711, 'items': None, 'time': '2020-03-09T09:58:53', 'created_time': '2020-03-09T09:58:53', 'isfile': True, 'isdir': False, 'owner': {'first_name': '', 'last_name': '', 'email': 'jesus.creatiboom@gmail.com', 'name': '', 'username': 'ejecutor', 'url': '/api/2.1/user/ejecutor/'}, 'acl': {'read': True, 'write': True, 'remove': True, 'list': True}, 'extension': 'jpg', 'mime': 'image/jpeg', 'tags': [], 'attributes': {}, 'has_preview': False, 'version': 0, 'lock': {'owner': None, 'islocked': False, 'id': None}, 'is_shared': False, 'uid': 113453960}]
+
+            print(obverse['url'])
+
+            obverseURL = collection.smartFileClient.get('/path/info', obverse['url'])
+
+            print(obverseURL)
 
         # Insert our data in the Google Spreadsheet.
-        insertData = [
-            request.form.get('type'),
-            request.form.get('name'),
-            request.form.get('obverse'),
-            request.form.get('reverse'),
-            request.form.get('country'),
-            request.form.get('denomination'),
-            request.form.get('date'),
-            request.form.get('diameter'),
-            request.form.get('composition'),
-            request.form.get('series'),
-            request.form.get('serial'),
-            request.form.get('grading'),
-            request.form.get('value'),
-            request.form.get('cost'),
-            prepareItemLink(request.form.get('name')),
-            request.form.get('mint'),
-        ]
+        # insertData = [
+        #     request.form.get('type'),
+        #     request.form.get('name'),
+        #     request.form.get('obverse'),
+        #     request.form.get('reverse'),
+        #     request.form.get('country'),
+        #     request.form.get('denomination'),
+        #     request.form.get('date'),
+        #     request.form.get('diameter'),
+        #     request.form.get('composition'),
+        #     request.form.get('series'),
+        #     request.form.get('serial'),
+        #     request.form.get('grading'),
+        #     request.form.get('value'),
+        #     request.form.get('cost'),
+        #     prepareItemLink(request.form.get('name')),
+        #     request.form.get('mint'),
+        # ]
 
-        rowCount = len(collection.googleData)
-        collection.sheet.insert_row(insertData, rowCount + 1)
+        # rowCount = len(collection.googleData)
+        # collection.sheet.insert_row(insertData, rowCount + 1)
 
-        return render_template('form-success.html', site = site)
+        # return render_template('form-success.html', site = site)
 
     return render_template('form.html', site = site)
 
