36b3186df5168ce9532c1538b93bdb0fb6692ada
Jesus A. Loranca Rubio <jesus.loranca@hroc.co.uk>
2020-03-18 16:57:46 +0000

    UPDATE: Slider config and styling to get closer to the desired look

--- static/js/main.js
+++ static/js/main.js
@@ -11,15 +11,7 @@ $(document).ready(function () {
     // Single item images slider.
     $('.slider').lightSlider({
         item: 1,
-        addClass: '',
-        mode: "slide",
-        useCSS: true,
-        cssEasing: 'ease',
-        easing: 'linear',
-        speed: 400,
-        slideEndAnimation: true,
         gallery: true,
-        galleryMargin: 5,
     });
 
     // Single item magnifiying effect.
