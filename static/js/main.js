289294c1d353c17fb4623a6f84724f0386e6e26e
Jesus A. Loranca Rubio <jesus.loranca@hroc.co.uk>
2020-03-18 16:18:06 +0000

    ADD: Initial slider code for single items

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,20 @@ $(document).ready(function () {
         });
     });
 
+    // Single item images slider.
+    $('.slider').lightSlider({
+        item: 1,
+        addClass: '',
+        mode: "slide",
+        useCSS: true,
+        cssEasing: 'ease',
+        easing: 'linear',
+        speed: 400,
+        slideEndAnimation: true,
+        gallery: true,
+        galleryMargin: 5,
+    });
+
     // Single item magnifiying effect.
     $('.item .zoom').each(function (index, element) {
         $(element).blowup({
